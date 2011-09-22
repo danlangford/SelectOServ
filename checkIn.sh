abf64571f87b93284f1f5d3d1ea80441f3f6d66b
danlangford <danlangford@hotmail.com>
2011-09-22 15:53:04 -0600

    checkIn script
    an example of a way to inform the server that your available given a
    certain process is running on your computer (POSIX)

---
+++ checkIn.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+procs=$(ps -ef | grep sso-config | wc -l); 
+until [ $procs -lt "2" ]; do
+       ipAddress=$(ifconfig utun0 | grep inet | awk '{print $2}');
+       curl -v  http://192.168.200.174:8181/checkIn?name=Andre\&ip=$ipAddress
+       procs=$(ps -ef | grep sso-config | wc -l); 
+       sleep 300
+done
+echo WAM Not running;
