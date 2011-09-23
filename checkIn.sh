24cd91041efa8912c5c50774a46d483c01566799
David Welch <david.welch@utahwd.com>
2011-09-23 07:22:53 -0600

    Fixed name in checkin file

--- checkIn.sh
+++ checkIn.sh
@@ -2,7 +2,7 @@
 procs=$(ps -ef | grep sso-config | wc -l); 
 until [ $procs -lt "2" ]; do
        ipAddress=$(ifconfig utun0 | grep inet | awk '{print $2}');
-       curl -v  http://192.168.200.174:8181/checkIn?name=Andre\&ip=$ipAddress
+       curl -v  http://192.168.200.174:8181/checkIn?name=$(whoami)\&ip=$ipAddress
        procs=$(ps -ef | grep sso-config | wc -l); 
        sleep 300
 done
